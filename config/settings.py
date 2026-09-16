f92609d9a6cafad5713cbdda7eaf0f83f5880ebd
kpsanda <guacanemeoscar18@gmail.com>
2026-09-16 17:08:22 +0000

    ci: validation test for automated build pipeline

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,8 @@ MAILERS = {
         "BACKEND": "django.core.mail.backends.console.EmailBackend",
     },
 }
+
+# ==========================================
+# CI/CD pipeline Validation Test _ Taller 8
+# ==========================================
+
